c8569cc835425348727e78b5a0d5db3d78ce56f5
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-03-15 23:27:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-function getFormvalue(event) {
-    event.preventDefault();
+document.getElementById("form1").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent page refresh
 
     let firstName = document.querySelector('input[name="fname"]').value.trim();
     let lastName = document.querySelector('input[name="lname"]').value.trim();
-
 
     if (!firstName || !lastName) {
         alert("Please enter both First Name and Last Name.");
         return;
     }
 
-
     alert(`${firstName} ${lastName}`);
-}
-
-
-document.getElementById("form1").addEventListener("submit", getFormvalue);
+});
